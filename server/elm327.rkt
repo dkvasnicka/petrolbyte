9de0562adbc150f40e1d75b730f306151bb774da
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2014-06-12 23:46:44 +0200

    Using streams to consume the port

--- server/elm327.rkt
+++ server/elm327.rkt
@@ -1,21 +1,34 @@
 #lang racket
 
-(require "utils.rkt")
+(require "utils.rkt"
+         racket/generator
+         srfi/41)
 
 (provide send-and-receive
          reset-device
          retrieve-dtcs
-         parse-dtcs)
+         parse-dtcs
+         response-stream)
 
 (define host "localhost")
 (define port 35000)
+
+(define [char-is-not c] (compose not (curry char=? c)))
+
+(define [response-stream iport original-cmd]
+  (let [[response-stream (stream-take-while (char-is-not #\>) 
+                                            (port->stream iport))]]
+    (stream-filter (char-is-not #\return)
+      (stream-drop (string-length original-cmd) response-stream))))
 
 (define [send-and-receive cmd [h host] [p port]]
   (call-with-values (λ [] (tcp-connect h p))
                     (λ [i o]
                        (display (string-append cmd "\r") o)
-                       (close-output-port o)
-                       (port->string i))))
+                       (flush-output o)
+                       (list->string 
+                         (stream->list 
+                           (response-stream i cmd))))))
 
 (define [reset-device]
   (send-and-receive "AT Z"))
--- server/tests/elm327.rkt
+++ server/tests/elm327.rkt
@@ -1,19 +1,28 @@
 #lang racket
 
 (require rackunit
-         "../elm327.rkt")
+         "../elm327.rkt"
+         srfi/41
+         rnrs/io/ports-6)
 
-(test-equal? "test that the TCP/IP function gets a proper response"                
-             (send-and-receive "AT Z")
+;(test-equal? "test that the TCP/IP function gets a proper response"                
+;             (send-and-receive "AT Z")
+;             "ELM327 v1.4b")
+;
+;(test-equal? "test that reseting the device produces device ID"                
+;             (reset-device)
+;             "ELM327 v1.4b")
+;
+;(test-equal? "test that error codes are properly retrieved and normalized to the Pxxxx format"                
+;             (retrieve-dtcs)
+;             '("P0182" "P2197"))
+
+(test-equal? "test that port is correctly consumed and parsed"
+             (list->string
+               (stream->list
+                 (response-stream 
+                   (open-string-input-port "AT Z\r\rELM327 v1.4b\r>") "AT Z")))
              "ELM327 v1.4b")
-
-(test-equal? "test that reseting the device produces device ID"                
-             (reset-device)
-             "ELM327 v1.4b")
-
-(test-equal? "test that error codes are properly retrieved and normalized to the Pxxxx format"                
-             (retrieve-dtcs)
-             '("P0182" "P2197"))
 
 (test-equal? "test that DTC parsing works"                
              (parse-dtcs "018221970000")
