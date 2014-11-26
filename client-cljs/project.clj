15b1e22231e97fbfc87363a9b12e855ba763d82b
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2014-11-26 21:44:23 +0100

    Cljs client lib versions update

--- client-cljs/project.clj
+++ client-cljs/project.clj
@@ -1,10 +1,11 @@
 (defproject client-cljs "0.1.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [jarohen/chord "0.4.2"]
                  [reagent "0.4.2"]
-                 [jayq "2.5.0"]]
+                 [jayq "2.5.2"]]
   
   :cljsbuild {
     :builds [{
