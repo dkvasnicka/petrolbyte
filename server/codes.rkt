d664529064d73cf2f33e03b1e3065ba0700ae6c4
dkvasnicka <daniel.kvasnicka.jr@gmail.com>
2015-07-04 21:13:15 +0200

    More rackety code

--- server/codes.rkt
+++ server/codes.rkt
@@ -5,6 +5,6 @@
 (provide trouble-codes)
 
 (define trouble-codes
-  (for/hash [[l (in-lines (open-file-input-port "codes.csv"))]]
-            (let [[data (string-split l "," #:trim? #f)]]
+  (for/hash ([l (in-lines (open-file-input-port "codes.csv"))])
+            (let ([data (string-split l "," #:trim? #f)])
               (values (car data) (string-join (cdr data) ",")))))
--- server/elm327.rkt
+++ server/elm327.rkt
@@ -12,15 +12,15 @@
 (define host "localhost")
 (define port 35000)
 
-(define [char-is-not c] (compose not (curry char=? c)))
+(define (char-is-not c) (compose not (curry char=? c)))
 
-(define [response-stream iport original-cmd]
-  (let [[response-stream (stream-take-while (char-is-not #\>) 
-                                            (port->stream iport))]]
+(define (response-stream iport original-cmd)
+  (let ([response-stream (stream-take-while (char-is-not #\>) 
+                                            (port->stream iport))])
     (stream-filter (char-is-not #\return)
       (stream-drop (string-length original-cmd) response-stream))))
 
-(define [send-and-receive cmd [h host] [p port]]
+(define (send-and-receive cmd [h host] [p port])
   (call-with-values (λ [] (tcp-connect h p))
                     (λ [i o]
                        (display (string-append cmd "\r") o)
@@ -29,17 +29,17 @@
                          (stream->list 
                            (response-stream i cmd))))))
 
-(define [reset-device]
+(define (reset-device)
   (send-and-receive "AT Z"))
 
-(define [retrieve-dtcs]
-  (let [[response (send-and-receive "03")]]
+(define (retrieve-dtcs)
+  (let ([response (send-and-receive "03")])
    (if (equal? (substring response 0 2) "43")
      (parse-dtcs (substring response 2))
      (error "Did not receive a proper DTC reply from the device!"))))
 
-(define [parse-dtcs dtcstring]
-  (let [[codebytes (partition-at 
-                      (remove* '(#\space) (string->list dtcstring)) 4)]]
+(define (parse-dtcs dtcstring)
+  (let ([codebytes (partition-at 
+                      (remove* '(#\space) (string->list dtcstring)) 4)])
     (map (λ [c] (string-append "P" (list->string c))) 
          (filter-not (compose null? (curry remove* '(#\0))) codebytes))))
